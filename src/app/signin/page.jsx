e7718c5301bb6136476b9c7316e35c23942c3a06
Bagus Suprapta <bagussuprapta@gmail.com>
2025-04-04 20:01:35 +0800

    refactor(auth): simplify and compact code structure for signin and signup pages

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -33,56 +33,54 @@ export default function Signin() {
   }
 
   return (
-    <div className="mt-20">
-      <div className="px-2 flex flex-col gap-y-3 justify-center items-center">
-        <AnimatePresence>
-          <div className="relative w-full flex justify-center">
-            {authMessage && (
-              <motion.p
-                key="authMessage"
-                initial={{ opacity: 0, y: -5 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: -5 }}
-                transition={{ duration: 0.3 }}
-                className="text-xs bg-[#dee3f0] px-2 text-[#54596a] rounded text-center absolute bottom-1"
-              >
-                {authMessage.charAt(0).toUpperCase() + authMessage.slice(1)}
-              </motion.p>
-            )}
-          </div>
-        </AnimatePresence>
-        <p>Signin</p>
-        <form onSubmit={handleSignIn}>
-          <div className="flex flex-col gap-y-2 w-full">
-            <input
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-              type="text"
-              placeholder="email"
-              className="border bg-white text-center font-mono text-xs w-full py-1 rounded-lg outline-none px-2"
-            />
-            <input
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-              type="password"
-              placeholder="password"
-              className="border bg-white text-center font-mono text-xs w-full py-1 rounded-lg outline-none px-2"
-            />
-          </div>
-          <div className="flex justify-center">
-            <button className="max-w-fit w-full mt-3 px-3 py-0.5 rounded-lg bg-[#826933] hover:bg-[#7b5e20] font-nunito text-[#eae8e3] font-medium cursor-pointer" type="submit">
-              {loading ? <p>Loading...</p> : <p>Login</p>}
-            </button>
-          </div>
-        </form>
-        <div>
-          <p className="text-center font-nunito">or</p>
-          <Link href="/signup" className="text-stone-500 hover:text-stone-800">
-            Signup
-          </Link>
+    <div className="mt-20 px-2 flex flex-col gap-y-3 justify-center items-center">
+      <AnimatePresence>
+        <div className="relative w-full flex justify-center">
+          {authMessage && (
+            <motion.p
+              key="authMessage"
+              initial={{ opacity: 0, y: -5 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -5 }}
+              transition={{ duration: 0.3 }}
+              className="text-xs bg-[#dee3f0] px-2 text-[#54596a] rounded text-center absolute bottom-1"
+            >
+              {authMessage.charAt(0).toUpperCase() + authMessage.slice(1)}
+            </motion.p>
+          )}
         </div>
+      </AnimatePresence>
+      <p>Signin</p>
+      <form onSubmit={handleSignIn}>
+        <div className="flex flex-col gap-y-2 w-full">
+          <input
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            type="text"
+            placeholder="email"
+            className="border bg-white text-center font-mono text-xs w-full py-1 rounded-lg outline-none px-2"
+          />
+          <input
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="password"
+            placeholder="password"
+            className="border bg-white text-center font-mono text-xs w-full py-1 rounded-lg outline-none px-2"
+          />
+        </div>
+        <div className="flex justify-center">
+          <button className="max-w-fit w-full mt-3 px-3 py-0.5 rounded-lg bg-[#826933] hover:bg-[#7b5e20] font-nunito text-[#eae8e3] font-medium cursor-pointer" type="submit">
+            {loading ? <p>Loading...</p> : <p>Login</p>}
+          </button>
+        </div>
+      </form>
+      <div>
+        <p className="text-center font-nunito">or</p>
+        <Link href="/signup" className="text-stone-500 hover:text-stone-800">
+          Signup
+        </Link>
       </div>
     </div>
   );
--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -32,58 +32,54 @@ export default function Signup() {
   }
 
   return (
-    <div>
-      <div className="mt-20">
-        <div className="px-2 flex flex-col gap-y-3 justify-center items-center">
-          <AnimatePresence>
-            <div className="relative w-full flex justify-center">
-              {authMessage && (
-                <motion.p
-                  key="authMessage"
-                  initial={{ opacity: 0, y: -5 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  exit={{ opacity: 0, y: -5 }}
-                  transition={{ duration: 0.3 }}
-                  className="text-xs bg-[#dee3f0] px-2 text-[#54596a] rounded text-center absolute bottom-1"
-                >
-                  {authMessage.charAt(0).toUpperCase() + authMessage.slice(1)}
-                </motion.p>
-              )}
-            </div>
-          </AnimatePresence>
-          <p>Signup</p>
-          <form onSubmit={handleSignup}>
-            <div className="flex flex-col gap-y-2 w-full">
-              <input
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-                type="text"
-                placeholder="email"
-                className="border bg-white text-center font-mono text-xs w-full py-1 rounded-lg outline-none px-2"
-              />
-              <input
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-                type="password"
-                placeholder="password"
-                className="border bg-white text-center font-mono text-xs w-full py-1 rounded-lg outline-none px-2"
-              />
-            </div>
-            <div className="flex justify-center">
-              <button className="max-w-fit mt-3 px-3 py-0.5 rounded-lg bg-[#826933] hover:bg-[#7b5e20] font-nunito text-[#eae8e3] font-medium cursor-pointer" type="submit">
-                {loading ? <p>Loading...</p> : <p>Register</p>}
-              </button>
-            </div>
-          </form>
-          <div>
-            <p className="text-center font-nunito">or</p>
-            <Link href="/signin" className="text-stone-500 hover:text-stone-800 ">
-              Signin
-            </Link>
-          </div>
+    <div className="mt-20 px-2 flex flex-col gap-y-3 justify-center items-center">
+      <AnimatePresence>
+        <div className="relative w-full flex justify-center">
+          {authMessage && (
+            <motion.p
+              key="authMessage"
+              initial={{ opacity: 0, y: -5 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -5 }}
+              transition={{ duration: 0.3 }}
+              className="text-xs bg-[#dee3f0] px-2 text-[#54596a] rounded text-center absolute bottom-1"
+            >
+              {authMessage.charAt(0).toUpperCase() + authMessage.slice(1)}
+            </motion.p>
+          )}
         </div>
+      </AnimatePresence>
+      <p>Signup</p>
+      <form onSubmit={handleSignup}>
+        <div className="flex flex-col gap-y-2 w-full">
+          <input
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            type="text"
+            placeholder="email"
+            className="border bg-white text-center font-mono text-xs w-full py-1 rounded-lg outline-none px-2"
+          />
+          <input
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="password"
+            placeholder="password"
+            className="border bg-white text-center font-mono text-xs w-full py-1 rounded-lg outline-none px-2"
+          />
+        </div>
+        <div className="flex justify-center">
+          <button className="max-w-fit mt-3 px-3 py-0.5 rounded-lg bg-[#826933] hover:bg-[#7b5e20] font-nunito text-[#eae8e3] font-medium cursor-pointer" type="submit">
+            {loading ? <p>Loading...</p> : <p>Register</p>}
+          </button>
+        </div>
+      </form>
+      <div>
+        <p className="text-center font-nunito">or</p>
+        <Link href="/signin" className="text-stone-500 hover:text-stone-800 ">
+          Signin
+        </Link>
       </div>
     </div>
   );
