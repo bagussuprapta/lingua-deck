e6a6350414bb71a2c51f331d437f4eef9d704ac3
Bagus Suprapta <bagussuprapta@gmail.com>
2025-04-06 02:27:07 +0800

    feat(ui): add spring animation to navbar on load

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,22 @@
 import { UserAuth } from "@/contexts/authContext";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+const baseInitial = { opacity: 0, y: -5 };
+const baseExit = { opacity: 0, y: -5, transition: { duration: 0.3 } };
+const createSpringAnimate = (delay = 0) => ({
+  opacity: 1,
+  y: 0,
+  transition: {
+    duration: 0.4,
+    delay,
+    ease: "easeOut",
+    type: "spring",
+    stiffness: 500,
+    damping: 20,
+  },
+});
 
 export default function Navbar() {
   const { isAuthenticated, signout } = UserAuth();
@@ -16,32 +32,36 @@ export default function Navbar() {
     await signout();
   }
 
-  if (!hydrated) {
-    return null;
-  }
+  if (!hydrated) return null;
 
   return (
-    <header className="w-full px-2 md:px-48 mt-4 font-nunito text-white">
+    <motion.header initial={baseInitial} animate={createSpringAnimate()} exit={baseExit} className="w-full px-2 md:px-48 mt-4 font-nunito text-white">
       <div className="flex gap-x-4 justify-between border-b-4 border-[#232222] bg-[#a4b9b5] rounded-2xl border-2 px-4 py-3">
         <div className="flex gap-x-4">
-          <Link href="/">Home</Link>
-          <Link href="/about">About</Link>
+          <motion.p initial={baseInitial} animate={createSpringAnimate(0.2)} exit={baseExit}>
+            <Link href="/">Home</Link>
+          </motion.p>
+          <motion.p initial={baseInitial} animate={createSpringAnimate(0.4)} exit={baseExit}>
+            <Link href="/about">About</Link>
+          </motion.p>
         </div>
         <div>
-          {isAuthenticated === "authenticating" ? (
-            <p>Loading...</p>
-          ) : isAuthenticated === "authenticated" ? (
+          {isAuthenticated === "authenticating" ? null : isAuthenticated === "authenticated" ? (
             <div className="flex gap-x-4">
-              <Link href="/profile">Profile</Link>
-              <p className="cursor-pointer" onClick={handleLogout}>
+              <motion.p initial={baseInitial} animate={createSpringAnimate(0.6)} exit={baseExit}>
+                <Link href="/profile">Profile</Link>
+              </motion.p>
+              <motion.p initial={baseInitial} animate={createSpringAnimate(0.8)} exit={baseExit} className="cursor-pointer" onClick={handleLogout}>
                 Signout
-              </p>
+              </motion.p>
             </div>
           ) : (
-            <Link href="/signin">Signin</Link>
+            <motion.p initial={baseInitial} animate={createSpringAnimate(0.6)} exit={baseExit}>
+              <Link href="/signin">Signin</Link>
+            </motion.p>
           )}
         </div>
       </div>
-    </header>
+    </motion.header>
   );
 }
