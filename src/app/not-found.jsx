950d5ca2b8e988d113e5cce6053771a4ee8af809
Bagus Suprapta <bagussuprapta@gmail.com>
2025-04-06 09:41:53 +0800

    fix(auth, layout): resolve layout issue on not found page and alert bugs on signup/signin

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center mt-20">
+    <div className="pt-32 flex flex-col items-center justify-center">
       <h1 className="font-bold geist-mono">404</h1>
       <p className="text-xs">oops! the page you’re looking for doesn’t exist.</p>
       <Link className="text-xs text-blue-400" href="/">
