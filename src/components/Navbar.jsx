037b6c51747fda737f060f3ab4e68f75f098dcc9
Bagus Suprapta <bagussuprapta@gmail.com>
2025-04-06 18:57:22 +0800

    style(navbar): add red color to logout and hover effect to links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@
 import { UserAuth } from "@/contexts/authContext";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 const baseInitial = { opacity: 0, y: -5 };
 const baseExit = { opacity: 0, y: -5, transition: { duration: 0.3 } };
@@ -35,29 +35,43 @@ export default function Navbar() {
   if (!hydrated) return null;
 
   return (
-    <motion.header initial={baseInitial} animate={createSpringAnimate()} exit={baseExit} className="w-full px-2 md:px-48 mt-4 font-nunito text-white fixed">
+    <motion.header initial={baseInitial} animate={createSpringAnimate()} exit={baseExit} className="w-full min-w-72 px-2 md:px-48 mt-4 font-nunito text-white fixed">
       <div className="flex gap-x-4 justify-between border-b-4 border-[#232222] bg-[#a4b9b5] rounded-2xl border-2 px-4 py-3">
         <div className="flex gap-x-4">
           <motion.p initial={baseInitial} animate={createSpringAnimate(0.2)} exit={baseExit}>
-            <Link href="/">Home</Link>
+            <Link href="/" className="hover:text-stone-100">
+              Home
+            </Link>
           </motion.p>
-          <motion.p initial={baseInitial} animate={createSpringAnimate(0.4)} exit={baseExit}>
-            <Link href="/about">About</Link>
+          <motion.p initial={baseInitial} animate={createSpringAnimate(0.3)} exit={baseExit}>
+            <Link href="/about" className="hover:text-stone-100">
+              About
+            </Link>
           </motion.p>
         </div>
         <div>
           {isAuthenticated === "authenticating" ? null : isAuthenticated === "authenticated" ? (
             <div className="flex gap-x-4">
-              <motion.p initial={baseInitial} animate={createSpringAnimate(0.6)} exit={baseExit}>
-                <Link href="/profile">Profile</Link>
+              <motion.p initial={baseInitial} animate={createSpringAnimate(0.4)} exit={baseExit}>
+                <Link href="/profile" className="hover:text-stone-100">
+                  Profile
+                </Link>
               </motion.p>
-              <motion.p initial={baseInitial} animate={createSpringAnimate(0.8)} exit={baseExit} className="cursor-pointer" onClick={handleLogout}>
+              <motion.p
+                initial={baseInitial}
+                animate={createSpringAnimate(0.5)}
+                exit={baseExit}
+                className="cursor-pointer bg-[#d5432c] hover:bg-[#c73922] hover:text-stone-100 px-2 rounded-xl"
+                onClick={handleLogout}
+              >
                 Signout
               </motion.p>
             </div>
           ) : (
-            <motion.p initial={baseInitial} animate={createSpringAnimate(0.6)} exit={baseExit}>
-              <Link href="/signin">Signin</Link>
+            <motion.p initial={baseInitial} animate={createSpringAnimate(0.4)} exit={baseExit}>
+              <Link href="/signin" className="hover:text-stone-100">
+                Signin
+              </Link>
             </motion.p>
           )}
         </div>
